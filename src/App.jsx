0a8f5ee768871d454788b8eb1480926c0710cbb5
fabricio rios <you@example.com>
2023-01-02 11:39:12 -0300

    Edição dos arquivos padrões do projeto.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+//Bibliotecas
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Router,
+  Navigate
+} from 'react-router-dom'
+
+//Páginas
+import Home from './Pages/Home/Home';
+
+//Components
+import Header from './componets/Header/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter> {/*Tag principal para universialização das rotas*/}
+      <Header/>
+      <div>
+        <Routes>{/*Páginas do App*/}
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
